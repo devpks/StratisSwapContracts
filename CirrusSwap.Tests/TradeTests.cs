16fec1f15a698c714b11ec1c3a734f59815fcf9c
Tyler Pena <tylerpena@me.com>
2019-11-18 20:01:15 -0800

    updated trade contract/began tests

--- CirrusSwap.Tests/TradeTests.cs
+++ CirrusSwap.Tests/TradeTests.cs
@@ -1,13 +1,79 @@
-using System;
+using Moq;
+using Stratis.SmartContracts.CLR;
+using Stratis.SmartContracts;
 using Xunit;
 
 namespace CirrusSwap.Tests
 {
     public class TradeTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Mock<ISmartContractState> MockContractState;
+        private readonly Mock<IPersistentState> MockPersistentState;
+        private readonly Mock<IContractLogger> MockContractLogger;
+        private readonly Mock<IInternalTransactionExecutor> MockInternalExecutor;
+        private readonly Address Owner;
+        private readonly Address TakerOne;
+        private readonly Address TakerTwo;
+        private readonly Address Token;
+        private readonly Address ContractAddress;
+        private readonly ulong Amount;
+        private readonly ulong Price;
+        private readonly bool IsActive;
+        private readonly string ContractType;
+
+        public TradeTests()
         {
+            MockContractLogger = new Mock<IContractLogger>();
+            MockPersistentState = new Mock<IPersistentState>();
+            MockContractState = new Mock<ISmartContractState>();
+            MockInternalExecutor = new Mock<IInternalTransactionExecutor>();
+            MockContractState.Setup(x => x.PersistentState).Returns(MockPersistentState.Object);
+            MockContractState.Setup(x => x.ContractLogger).Returns(MockContractLogger.Object);
+            MockContractState.Setup(x => x.InternalTransactionExecutor).Returns(MockInternalExecutor.Object);
+            Owner = "0x0000000000000000000000000000000000000001".HexToAddress();
+            TakerOne = "0x0000000000000000000000000000000000000002".HexToAddress();
+            TakerTwo = "0x0000000000000000000000000000000000000003".HexToAddress();
+            Token = "0x0000000000000000000000000000000000000004".HexToAddress();
+            ContractAddress = "0x0000000000000000000000000000000000000005".HexToAddress();
+        }
+
+        private Trade NewTrade(Address sender, ulong value, string contractType, Address token, ulong amount, ulong price)
+        {
+            MockContractState.Setup(x => x.Message).Returns(new Message(ContractAddress, sender, 0));
+            MockPersistentState.Setup(x => x.GetAddress(nameof(Owner))).Returns(Owner);
+            MockPersistentState.Setup(x => x.GetString(nameof(ContractType))).Returns(contractType);
+            MockPersistentState.Setup(x => x.GetAddress(nameof(Token))).Returns(token);
+            MockPersistentState.Setup(x => x.GetUInt64(nameof(Amount))).Returns(amount);
+            MockPersistentState.Setup(x => x.GetUInt64(nameof(Price))).Returns(price);
+            MockPersistentState.Setup(x => x.GetBool(nameof(IsActive))).Returns(true);
+
+            return new Trade(MockContractState.Object, contractType, token, amount, price);
+        }
+
+        [Theory]
+        [InlineData(0, 10_000_000, 5_000_000_000, "sell")]
+        [InlineData(5, 10_000_000, 5_000_000_000, "buy")]
+        public void Creates_New_Trade(ulong value, ulong price, ulong amount, string type)
+        {
+            var trade = NewTrade(Owner, value, type, Token, amount, price);
+
+            MockPersistentState.Verify(x => x.SetAddress(nameof(Owner), Owner));
+            Assert.Equal(Owner, trade.Owner);
+            
+            MockPersistentState.Verify(x => x.SetAddress(nameof(Token), Token));
+            Assert.Equal(Token, trade.Token);
+
+            MockPersistentState.Verify(x => x.SetUInt64(nameof(Price), price));
+            Assert.Equal(price, trade.Price);
+
+            MockPersistentState.Verify(x => x.SetUInt64(nameof(Amount), amount));
+            Assert.Equal(amount, trade.Amount);
+
+            MockPersistentState.Verify(x => x.SetBool(nameof(IsActive), true));
+            Assert.Equal(true, trade.IsActive);
+
+            MockPersistentState.Verify(x => x.SetString(nameof(ContractType), type));
+            Assert.Equal(type, trade.ContractType);
         }
     }
 }
--- CirrusSwap/Contracts/Trade.cs
+++ CirrusSwap/Contracts/Trade.cs
@@ -7,81 +7,36 @@ public class Trade : SmartContract
     private const string sell = "sell";
     public Trade(
         ISmartContractState smartContractState, 
-        string tradeAction, 
+        string contractType, 
         Address token, 
         ulong amount, 
         ulong price) : base (smartContractState)
     {
-        var action = tradeAction.ToLowerInvariant();
-        Assert(action == buy || action == sell, "Action must me buy or sell");
-        Assert(amount > 0, "Amount must be greater than 0");
+        contractType = contractType.ToLowerInvariant();
+        Assert(contractType == buy || contractType == sell, "Action must me buy or sell");
+        Assert(amount > 100000000, "Amount must be greater than 0");
         Assert(price > 0, "Price must be greater than 0");
 
         this.Token = token;
         this.Owner = Message.Sender;
-        this.OwnerAction = action;
-        this.Amount = amount;
-        this.Price = price;
-        this.TotalPrice = amount * price;
+        this.ContractType = contractType;
+        this.Amount = amount; // amount of src always in full e.g. (50adt)
+        this.Price = price; // price per src in stratoshies e.g. 10_000_000 (.1crs)
 
-        if (action == buy) 
+        if (contractType == buy) 
         {
-            Assert(VerifyBuyAction());
-        }
-        else if (action == sell)
-        {
-            Assert(VerifySellAction());
+            Assert(Message.Value > this.TradeBalance);
         }
 
         this.IsActive = true;
     }
 
-    private bool VerifyBuyAction()
-    {
-        // Verify the value sent covers the amount to buy and purchase price
-        Assert(Message.Value > this.TotalPrice);
+    public ulong TradeBalance => this.Amount / this.Price;
 
-        return true;
-    }
-
-    private bool VerifySellAction()
-    {
-        // Verify the balance of this contracts address
-        var result = Call(this.Token, 0, "Allowance", new object[] { });
-
-        Assert(result.Success);
-
-        Assert((ulong)result.ReturnValue >= this.TotalPrice);
-
-        return true;
-    }
-
-    public void Destroy()
-    {
-        Assert(Message.Sender == Owner);
-
-        if (this.OwnerAction == buy)
-        {
-            // Need to keep an updated balance through txs
-            this.Transfer(this.Owner, this.Amount * this.Price);
-        }
-        else if (this.OwnerAction == sell)
-        {
-            // Need to keep an updated balance through txs
-            var result = Call(this.Token, 0, "TransferTo", new object[] { this.Owner, this.Amount * this.Price });
-
-            Assert(result.Success);
-        }
-
-        this.IsActive = false;
-    }
-
-    public ulong TotalPrice => this.Price * this.Amount;
-
-    private bool IsActive 
+    public bool IsActive 
     {
         get => PersistentState.GetBool(nameof(IsActive));
-        set => PersistentState.SetBool(nameof(IsActive), value);
+        private set => PersistentState.SetBool(nameof(IsActive), value);
     }
 
     public Address Token {
@@ -89,9 +44,9 @@ public class Trade : SmartContract
         private set => PersistentState.SetAddress(nameof(Token), value);
     }
 
-    public string OwnerAction {
-        get => PersistentState.GetString(nameof(OwnerAction));
-        private set => PersistentState.SetString(nameof(OwnerAction), value);
+    public string ContractType {
+        get => PersistentState.GetString(nameof(ContractType));
+        private set => PersistentState.SetString(nameof(ContractType), value);
     }
 
     public ulong Price {
@@ -109,59 +64,49 @@ public class Trade : SmartContract
         private set => PersistentState.SetAddress(nameof(Owner), value);
     }
 
-    public Buyer GetBuyer(Address buyer) {
-        return PersistentState.GetStruct<Buyer>($"Buyer:{buyer}");
-    }
-
-    private void SetBuyer(Buyer buyer) {
-        PersistentState.SetStruct($"Buyer:{buyer.Address}", buyer);
-    }
-
-    public Seller GetSeller(Address seller) {
-        return PersistentState.GetStruct<Seller>($"Seller:{seller}");
-    }
-
-    private void SetSeller(Seller seller) {
-        PersistentState.SetStruct($"Seller:{seller.Address}", seller);
-    }
-
-    #region Transactions
-    public Transaction[] GetTransactions()
+    public void Destroy()
     {
-        return PersistentState.GetArray<Transaction>("Transactions");
-    }
+        Assert(Message.Sender == Owner);
 
-    private void SetNewTransaction(Transaction transaction) 
-    {
-        var transactions = GetTransactions();
-        var newLength = transactions.Length + 1;
-        var newTransactions = new Transaction[newLength];
-
-        for (var i = 0; i < transactions.Length; i++)
+        if (this.ContractType == buy)
         {
-            newTransactions[i] = transactions[i];
+            // Need to keep an updated balance through txs
+            this.Transfer(this.Owner, this.Amount * this.Price);
+        }
+        else if (this.ContractType == sell)
+        {
+            // Need to keep an updated balance through txs
+            var result = Call(this.Token, 0, "TransferTo", new object[] { this.Owner, this.Amount * this.Price });
+
+            Assert(result.Success);
         }
 
-        newTransactions[newLength] = transaction;
-
-        PersistentState.SetArray("Transactions", newTransactions);
+        this.IsActive = false;
     }
-    #endregion
 
-    public Transaction Buy(ulong amount)
+    public Transaction Buy(ulong amount) // amount of src to buy in full e.g. (50 adt)
     {
+        // Verify
         Assert(Message.Sender != Owner);
-        Assert(OwnerAction != "buy", "Invalid operation, sell offers only.");
+        Assert(ContractType == sell, "Invalid operation, buy offers only.");
+        Assert(amount <= Amount, "Cannot purchase more than what is beings sold.");
 
         var totalCost = amount * Price;
-        // Verify
-        Assert(amount <= Amount, "Cannot purchase more than what is beings sold");
         Assert(Message.Value >= totalCost, "Not enough funds to cover purchase.");
 
+        // Convert amount from full amount to stratoshies
+        // e.g. from 50 to 5_000_000_000
+        ulong stratoshiAmount = 50 * 10000000;
+
         // Transfer SRC
-        var transferResult = Call(Token, 0, "TransferFrom", new object[] {Owner, Message.Sender, amount});
+        // Contract address must have necessary allowance
+        var transferResult = Call(Token, 0, "TransferFrom", new object[] { Owner, Message.Sender, stratoshiAmount });
+        
         // Verify Successful Transfer
-        Assert(transferResult.Success);
+        if (!transferResult.Success)
+        {
+            this.Destroy();
+        }
 
         // Transfer CRS totalCost to seller
         Transfer(Owner, totalCost);
@@ -172,61 +117,42 @@ public class Trade : SmartContract
             Transfer(Message.Sender, Balance);
         }
 
-        // Create, Set Buyer
-        Buyer buyer = NewBuyer(Message.Sender, Token, amount, totalCost, Balance);
-        SetBuyer(buyer);
+        this.Amount -= amount;
 
-        // Get, Edit, Set Seller
-        Seller seller = GetSeller(Owner);
-        seller.AmountRecieved = totalCost;
-        seller.AmountSold = amount;
-        seller.Balance -= amount;
-        SetSeller(seller);
+        // if (this.Amount == 0)
+        // {
+        //     this.Destroy();
+        // }
 
-        // Build Transaction Result
-        var txResult = new Transaction { Buyer = buyer, Seller = seller };
-
-        // Save TransactionResult
-        SetNewTransaction(txResult);
+        // Log Transaction Result
+        var txResult = new Transaction { Buyer = Message.Sender, Seller = Owner, SrcAmount = stratoshiAmount, CrsAmount = totalCost };
+        Log(txResult);
         
         return txResult;
     }
 
-    public Transaction Sell(ulong amount)
+    // public Transaction Sell(ulong amount)
+    // {
+    //     Assert(Message.Sender != Owner);
+    //     Assert(ContractType != "sell", "Invalid operation, buy offers only.");
+
+    //     var txResult = new Transaction { Buyer = Message.Sender, Seller = Owner, SrcAmount = stratoshiAmount, CrsAmount = totalCost };
+    //     Log(txResult);
+
+    //     return txResult;
+    // }
+
+    private ulong ToFull(ulong a)
     {
-        Assert(Message.Sender != Owner);
-        Assert(OwnerAction != "sell", "Invalid operation, buy offers only.");
-
-        var txResult = new Transaction { Buyer = GetBuyer(Message.Sender), Seller = GetSeller(Owner) };
-
-        return txResult;
+        // a / 100000000 = 50
+        return a / 100000000;
     }
 
-    #region Helpers
-    private Buyer NewBuyer(
-        Address address, Address token, ulong amountPurchasd, ulong amountSpent, ulong balance)
+    public struct Transaction
     {
-        return new Buyer 
-        {
-            Address = address,
-            Token = token,
-            AmountPurchased = amountPurchasd,
-            AmountSpent = amountSpent,
-            Balance = balance
-        };
+        public Address Buyer;
+        public Address Seller;
+        public ulong CrsAmount; // total amount traded
+        public ulong SrcAmount; // total amount traded
     }
-
-    private Seller NewSeller(
-        Address address, Address token, ulong amountSold, ulong amountRevieved, ulong balance)
-    {
-        return new Seller 
-        {
-            Address = address,
-            Token = token,
-            AmountSold = amountSold,
-            AmountRecieved = amountRevieved,
-            Balance = balance
-        };
-    }
-    #endregion
 }
