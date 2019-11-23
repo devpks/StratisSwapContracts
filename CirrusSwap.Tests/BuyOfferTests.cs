365e48d30d2eab41f66e243b8c381e0f16d3bcce
Tyler Pena <tylerpena@me.com>
2019-11-23 12:47:55 -0800

    Begin buy offer tests

--- CirrusSwap.Tests/BuyOfferTests.cs
+++ CirrusSwap.Tests/BuyOfferTests.cs
@@ -2,6 +2,8 @@ using Moq;
 using Stratis.SmartContracts.CLR;
 using Stratis.SmartContracts;
 using Xunit;
+using static BuyOffer;
+using Castle.DynamicProxy.Contributors;
 
 namespace CirrusSwap.Tests
 {
@@ -39,6 +41,7 @@ namespace CirrusSwap.Tests
         private BuyOffer NewBuyOffer(Address sender, ulong value, ulong amount, ulong price)
         {
             MockContractState.Setup(x => x.Message).Returns(new Message(ContractAddress, sender, value));
+            MockContractState.Setup(x => x.GetBalance).Returns(() => value);
             MockPersistentState.Setup(x => x.GetAddress(nameof(Buyer))).Returns(Buyer);
             MockPersistentState.Setup(x => x.GetAddress(nameof(TokenAddress))).Returns(TokenAddress);
             MockPersistentState.Setup(x => x.GetUInt64(nameof(TokenAmount))).Returns(amount);
@@ -49,26 +52,84 @@ namespace CirrusSwap.Tests
         }
 
         [Theory]
-        [InlineData(0, 10_000_000, 5_000_000_000)]
-        [InlineData(500_000_000, 10_000_000, 5_000_000_000)]
+        [InlineData(500_000_000, 10_000_000, 50)]
+        [InlineData(50_000_000, 10_000_000, 5)]
         public void Creates_New_Trade(ulong value, ulong price, ulong amount)
         {
             var trade = NewBuyOffer(Buyer, value, amount, price);
 
-            MockPersistentState.Verify(x => x.SetAddress(nameof(Buyer), Buyer));
+            MockPersistentState.Verify(x => x.SetAddress(nameof(Buyer), Buyer), Times.Once);
             Assert.Equal(Buyer, trade.Buyer);
             
-            MockPersistentState.Verify(x => x.SetAddress(nameof(TokenAddress), TokenAddress));
+            MockPersistentState.Verify(x => x.SetAddress(nameof(TokenAddress), TokenAddress), Times.Once);
             Assert.Equal(TokenAddress, trade.TokenAddress);
 
-            MockPersistentState.Verify(x => x.SetUInt64(nameof(TokenPrice), price));
+            MockPersistentState.Verify(x => x.SetUInt64(nameof(TokenPrice), price), Times.Once);
             Assert.Equal(price, trade.TokenPrice);
 
-            MockPersistentState.Verify(x => x.SetUInt64(nameof(TokenAmount), amount));
+            MockPersistentState.Verify(x => x.SetUInt64(nameof(TokenAmount), amount), Times.Once);
             Assert.Equal(amount, trade.TokenAmount);
 
-            MockPersistentState.Verify(x => x.SetBool(nameof(IsActive), true));
+            MockPersistentState.Verify(x => x.SetBool(nameof(IsActive), true), Times.Once);
             Assert.Equal(true, trade.IsActive);
+        }
+
+        [Theory]
+        [InlineData(49_999_999, 10_000_000, 5)]
+        [InlineData(0, 10_000_000, 50)]
+        [InlineData(5_000_000_000, 0, 50)]
+        [InlineData(5_000_000_000, 10_000_000, 0)]
+        public void Create_NewTrade_Fails_Invalid_Parameters(ulong value, ulong price, ulong amount)
+        {
+            MockContractState.Setup(x => x.Message).Returns(new Message(ContractAddress, Buyer, value));
+
+            Assert.ThrowsAny<SmartContractAssertException>(() => new BuyOffer(MockContractState.Object, TokenAddress, amount, price));
+        }
+
+        [Theory]
+        [InlineData(500_000_000, 50, 10_000_000)]
+        public void Success_GetTradeDetails(ulong value, ulong price, ulong amount)
+        {
+            var trade = NewBuyOffer(Buyer, value, amount, price);
+            var actualTradeDetails = trade.GetTradeDetails();
+            var expectedTradeDetails = new TradeDetails
+            {
+                IsActive = true,
+                TokenAmount = amount,
+                TokenPrice = price,
+                TokenAddress = TokenAddress,
+                ContractBalance = value
+            };
+
+
+            Assert.Equal(expectedTradeDetails, actualTradeDetails);
+        }
+
+        [Fact]
+        public void CloseTrade_Failure_If_Sender_IsNot_Owner()
+        {
+            var trade = NewBuyOffer(Buyer, 1, 1, 1);
+
+            MockContractState.Setup(x => x.Message).Returns(new Message(ContractAddress, SellerOne, 0));
+
+            Assert.ThrowsAny<SmartContractAssertException>(() => trade.CloseTrade());
+        }
+
+        [Fact]
+        public void CloseTrade_Success_If_Sender_IsNot_Owner()
+        {
+            var trade = NewBuyOffer(Buyer, 1, 1, 1);
+
+            trade.CloseTrade();
+
+            MockContractState.Verify(x => x.GetBalance, Times.AtLeastOnce);
+            MockPersistentState.Verify(x => x.SetBool(nameof(IsActive), false), Times.Once);
+
+            MockContractState.Setup(x => x.GetBalance).Returns(() => 0);
+            MockPersistentState.Setup(x => x.GetBool(nameof(IsActive))).Returns(false);
+
+            Assert.Equal((ulong)0, trade.Balance);
+            Assert.Equal(false, trade.IsActive);
         }
     }
 }
