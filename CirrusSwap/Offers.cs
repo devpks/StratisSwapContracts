1d83a11a17e0805a6f198f211830f1e758f619e4
Tyler Pena <tylerpena@me.com>
2019-11-28 11:45:39 -0800

    Simple offers logging contract

---
+++ CirrusSwap/Offers.cs
@@ -0,0 +1,35 @@
+﻿using Stratis.SmartContracts;
+
+[Deploy]
+public class Offers : SmartContract
+{
+    public Offers(ISmartContractState smartContractState) : base(smartContractState)
+    { }
+
+    public void AddOffer(string tradeAction, ulong tokenAmount, ulong tokenPrice,
+        Address tokenAddress, Address contractAddress)
+    {
+        Log(new Offer
+        {
+            Owner = Message.Sender,
+            TradeAction = tradeAction,
+            TokenAmount = tokenAmount,
+            TokenPrice = tokenPrice,
+            TokenAddress = tokenAddress,
+            ContractAddress = contractAddress
+        });
+    }
+
+    public struct Offer
+    {
+        [Index]
+        public Address Owner;
+        [Index]
+        public Address TokenAddress;
+        [Index]
+        public Address ContractAddress;
+        public ulong TokenAmount;
+        public ulong TokenPrice;
+        public string TradeAction;
+    }
+}
